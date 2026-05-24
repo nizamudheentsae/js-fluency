df35ab4314cfb6cbf6cfaf60e8090fa9aabe4e9e
nizamudheen <nizamudheents.ae@gmail.com>
2026-05-24 16:17:35 +0400

    Day 01 - Variables and Types

---
+++ app.js
@@ -0,0 +1,32 @@
+
+
+let name = "nizam";
+let age = 28;
+let isWorking = true;
+
+console.log(name);
+console.log(age);
+console.log(isWorking);
+
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isWorking);
+
+console.log("5" + 5);
+console.log("5" - 5);
+console.log(true + true);
+console.log(false + 1);
+
+let currentJob = "Accountant";
+let location = "Dubai";
+let targetRole = "MERN Developer";
+
+
+console.log(
+    "i am an " +
+    currentJob + 
+    " in "+     location +
+    "preparing for to become a "+
+    targetRole
+);
