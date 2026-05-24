2e9889a0fa3b357b2bf2aa334b2543afde0e6c82
nizamudheen <nizamudheents.ae@gmail.com>
2026-05-24 19:37:58 +0400

    Restructure repository and add Day 02 - Functions

---
+++ day2/app.js
@@ -0,0 +1,73 @@
+
+
+// function greet (name) {
+//     console.log("hello " + name);
+// }
+
+// greet("Nizam");
+// greet("Ahmad");
+// greet("Ali");
+
+
+// function add(a,b) {
+//     return a+b;
+// }
+
+// let result = add(10,20);
+
+// console.log(result);
+
+
+// function add(a,b) {
+//     return a+b;
+// }
+// let result = add(10,20);
+
+// console.log(result);
+
+
+// function multiply(a,b) {
+//     return a*b;
+// }
+
+// let answer = multiply(5,4);
+
+// console.log(answer);
+
+
+// const multiply = function (a,b) {
+//     return a*b;
+
+// }
+
+// console.log(multiply(6,5));
+
+
+
+// const multiply = (a,b) => {
+//     return a * b;
+// }
+
+// console.log(multiply(10,5));
+
+
+
+
+// const sayJob = () => {
+//     console.log(" I am Accountat");
+// }
+
+// sayJob();
+
+// const sayCountry = (Country) => {
+//     console.log ("I Work in " + Country);
+// }
+// sayCountry("UAE");
+
+
+
+// const calculateVat = (price) => {
+//     return price * 5/100;
+// }
+
+// console.log(calculateVat(1000));
