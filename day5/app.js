54c601b925d54c7135fdd53b5611c7320f48bdbd
nizamudheen <nizamudheents.ae@gmail.com>
2026-05-26 01:00:00 +0400

    Day 05 - Objects

---
+++ day5/app.js
@@ -0,0 +1,70 @@
+
+
+
+// const user = {
+//     name: "Nizam",
+//     job: "Accountatnt",
+//     country: "UAE"
+// }
+
+// console.log(user);
+
+// console.log(user.name);
+// console.log(user.job);
+// console.log(user.country);
+
+
+
+// const user = {
+//     name: "Nizam",
+//     job: "Accountatnt",
+//     country: "UAE"
+// }
+
+// user.job = "MERN Developer";
+
+// console.log(user);
+
+
+
+// const user = {
+//     name: "Nizam",
+//     job: "MERN Developer",
+//     country: "UAE"
+// }
+
+// user.salary = 6000;
+
+// console.log(user);
+
+
+// const users = [
+//     {
+//         name: "Nizam",
+//         job: "Accountant",
+// },
+// {
+//     name: "Ahmad",
+//     job: "Developer",
+    
+// }
+// ];
+
+// console.log(users);
+
+
+
+const users = [
+    {
+        name: "Nizam",
+        job: "Accountant",
+},
+{
+    name: "Ahmad",
+    job: "Developer", 
+}  
+];
+
+for (let i = 0; i < users.length; i++) {
+    console.log(users[i].name);
+}
