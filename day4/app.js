f253a457c73d6880d571550cc3a1dcd138cea7cb
nizamudheen <nizamudheents.ae@gmail.com>
2026-05-26 00:41:02 +0400

    Day 04 - Arrays

---
+++ day4/app.js
@@ -0,0 +1,44 @@
+
+
+// let fruits =["apple", "mango", "orange"];
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+
+
+// let fruits =["apple", "mango", "orange"];
+
+// fruits.push("banana");
+
+// console.log(fruits);
+
+
+// let fruits = ["apple", "mango", "orange", "banana"];
+
+// fruits.pop();
+
+// console.log(fruits);
+
+
+// let fruits = ["apple", "mango", "orange"];
+
+// fruits.unshift("grapes");
+
+// console.log(fruits);
+
+// let fruits = ["grapes", "apple", "mango", "orange"];
+
+// fruits.shift();
+
+// console.log(fruits);
+// console.log(fruits[0]);
+// console.log(fruits.length);
+
+
+
+let fruits = ["apple", "mango", "orange"];
+
+for (let i = 0; i <fruits.length; i++) {
+    console.log(fruits[i]);
+}
