bd0e046e80d6fe447684b1badd37fd89a6185be5
nizamudheen <nizamudheents.ae@gmail.com>
2026-05-26 21:30:14 +0400

    Day 10 - String Methods and Template Literals

---
+++ day10/app.js
@@ -0,0 +1,44 @@
+
+// let name = "Nizam";
+
+// console.log(name.toUpperCase());
+// console.log(name.toLowerCase());
+
+
+// let username = "   nizam";
+
+// console.log(username);
+// console.log(username.trim());
+
+
+// let email = "nizam@gmail.com";
+
+// console.log(email.includes("@"));
+// console.log(email.includes("gmail"));
+// console.log(email.includes("yahoo")); 
+
+// let fullName ="Nizamudheen TS";
+
+// let parts = fullName.split(" ");
+
+// console.log(parts);
+// console.log(parts[0]);
+// console.log(parts[1]);
+
+
+// let name = "Nizam";
+// let job = "MERN Developer";
+// let country = "UAE";
+
+// let message = `My name is ${name}. I am a ${job} in ${country}.`;
+
+// console.log(message);
+
+
+let email = "  NIZAM@GMAIL.COM  ";
+
+let cleanEmail = email.trim().toLowerCase();
+
+console.log(cleanEmail);
+console.log(cleanEmail.includes("@"));
+console.log(cleanEmail.includes("gmail"));
