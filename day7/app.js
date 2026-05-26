b5e65683ec9d5973e348d69b4929db916e3f79d2
nizamudheen <nizamudheents.ae@gmail.com>
2026-05-26 13:46:44 +0400

    Day 07 - Loops

---
+++ day7/app.js
@@ -0,0 +1,40 @@
+
+
+// for (let i = 1; i <=5; i++) {
+//     console.log(i);
+// }
+
+
+
+// let fruits = ['apple', 'mango', 'orange'];
+
+// for (let i = 0; i <fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+
+
+// let fruits = ['apple', 'mango', 'orange'];
+
+// for (let i = 0; i <=fruits.length; i++) {
+//     console.log(fruits[i]);
+// }
+
+// const users = [
+//     {name: "Nizam", job: "Accountatnt"},
+//     {name: "Ahmad", job: "Developer"},
+//     {name: "Ali", job: "Designer"}
+// ];
+
+// for (let i = 0; i <users.length; i++) {
+//     console.log(users[i].name);
+// }
+
+
+for (let i =1; i <=10; i++) {
+    if (i=== 5) {
+        break
+
+    }
+    console.log(i);
+}
