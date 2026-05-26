d3346e28ad6f1a497511605e8aa8a69f21abee37
nizamudheen <nizamudheents.ae@gmail.com>
2026-05-26 11:40:46 +0400

    Day 06 - Conditions and Operators

---
+++ day6/app.js
@@ -0,0 +1,67 @@
+
+
+// let salary = 4000;
+
+// if (salary >= 5000) {
+//     console.log("Good Salary");
+// }
+
+
+
+
+// let salary = 6000;
+
+// if (salary >= 5000) {
+//     console.log("Good Salary");
+// } else {
+//     console.log("Need Better Salary")
+// }
+
+
+
+// let country = "India";
+
+// if (country === "") {
+//     console.log("Welcome to UAE");
+
+// } else {
+//     console.log("access Denied");
+// // }
+
+
+// let country = "UAE";
+
+// if (country !== "UAE") {
+//     console.log("Foreign Country")
+
+// } else {
+//     console.log("UAE Resident");
+// }
+
+
+// let salary = 6000;
+
+// let experiance = 1;
+
+// if (salary >= 5000 && experiance >= 2) {
+//     console.log("Eligible")
+// } else {
+//     console.log("not Eligible")
+// }
+
+
+// let role = "user";
+
+// if (role === "Admin" || role === "manager") {
+//     console.log("Access Granted");
+// } else {
+//     console.log("Access Denied");
+// }
+
+
+let salary = 6000;
+let experience = 1;
+
+if (salary >= 5000 || experience >= 2) {
+    console.log("Selected");
+}
