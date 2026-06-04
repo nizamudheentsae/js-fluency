cf36c3aa5568729d7a0c02682db334636f31b644
nizamudheen <nizamudheents.ae@gmail.com>
2026-06-04 20:47:08 +0400

    Day 27 - ES Modules

---
+++ day27/app.js
@@ -0,0 +1,12 @@
+import { add, subtract } from "./math.js";
+import { userName } from "./math.js";
+import {multiply} from "./math.js";
+
+console.log(multiply(10, 5));
+
+console.log(userName);
+
+console.log(add(10, 5));
+
+console.log(subtract(10, 5));
+
---
+++ day27/math.js
@@ -0,0 +1,15 @@
+export function add(a, b) {
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
+}
+
+
+export const userName = "Nizam";
+
+
+export function multiply(a, b) {
+    return a * b;
+}
